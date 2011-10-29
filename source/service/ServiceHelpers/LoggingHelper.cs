61e4191f46194589546b64a529fd569b5be5c5f0
Omri Gazitt <ogazitt@gmail.com>
2011-10-28 22:35:55 -0700

    TaskStore directory reorganization, part 2. Created a source directory with three subdirectories: 	service: has all the service projects 	winphone: has all the WP7 projects 	clients: has all the test clients (that operate against the service) Each of these directories also has a "tests" subdirectory that has the corresponding test projects. All the .csproj files have been rewritten to output object files to an obj directory (a peer of source) and the binary files to a bin directory.
    With all these changes, source control using git should be more
    straightforward - there will be a far higher degree of likelihood that git
    commit -a will work fine, and that the "untracked" files (besides bin and
    obj) will be relevant to add to the project.

---
+++ source/service/ServiceHelpers/LoggingHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace ServiceHelpers
+{
+    public class LoggingHelper
+    {
+        public enum LogLevel
+        {
+            Fatal,
+            Error,
+            Info,
+            Detail
+        }
+
+        public static void TraceLine(string message, LogLevel level)
+        {
+            TraceLine(message, LevelText(level));
+        }
+
+        public static void TraceLine(string message, string level)
+        {
+            string msg = String.Format(
+                    "{0}: {1}",
+                    DateTime.Now.ToString(),
+                    message);
+
+            if (RoleEnvironment.IsAvailable)
+            {
+                Trace.WriteLine(msg, level);
+                Trace.Flush();
+            }
+            else
+                Console.WriteLine(msg);
+        }
+
+        // do not compile this in unless this is a DEBUG build
+        [Conditional("DEBUG")]
+        public static void TraceFunction()
+        {
+            StackTrace st = new StackTrace(true);
+            StackFrame sf = st.GetFrame(1);
+            string msg = String.Format(
+                "Entering {0} {1}:{2}",
+                sf.GetFileName(),
+                sf.GetMethod().Name,
+                sf.GetFileLineNumber());
+            TraceLine(msg, LogLevel.Detail);
+        }
+
+        public static void TraceInfo(string message)
+        {
+            StackTrace st = new StackTrace(true);
+            StackFrame sf = st.GetFrame(1);
+            string msg = String.Format(
+                "Info from {0} {1}:{2}; {3}",
+                sf.GetFileName(),
+                sf.GetMethod().Name,
+                sf.GetFileLineNumber(),
+                message);
+            TraceLine(msg, LogLevel.Info);
+        }
+
+        public static void TraceError(string message)
+        {
+            StackTrace st = new StackTrace(true);
+            StackFrame sf = st.GetFrame(1);
+            string msg = String.Format(
+                "Error in {0} {1}:{2}; {3}",
+                sf.GetFileName(),
+                sf.GetMethod().Name,
+                sf.GetFileLineNumber(),
+                message);
+            TraceLine(msg, LogLevel.Error);
+        }
+
+        public static void TraceFatal(string message)
+        {
+            StackTrace st = new StackTrace(true);
+            StackFrame sf = st.GetFrame(1);
+            string msg = String.Format(
+                "***Fatal Error*** in {0} {1}:{2}; {3}",
+                sf.GetFileName(),
+                sf.GetMethod().Name,
+                sf.GetFileLineNumber(),
+                message);
+            TraceLine(msg, LogLevel.Fatal);
+        }
+
+        private static string LevelText(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Fatal:
+                    return "Fatal Error";
+                case LogLevel.Error:
+                    return "Error";
+                case LogLevel.Info:
+                    return "Information";
+                case LogLevel.Detail:
+                    return "Detail";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
